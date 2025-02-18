44e9a21ab9c770d38f43568a449865a583994f78
WaleedAls <swyd16@gmail.com>
2025-02-18 19:37:02 +0300

    Waleed Alshammari

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,25 @@
+import kotlin.math.absoluteValue
+
 fun main() {
     println("Hello World!")
+/*
+    // 1.
+var nonValue: Int = 35
+    nonValue = null
+    println("The value is ${nonValue}")
+
+   //2.
+   var city: String? = null
+    //3. //4.
+    var lenght = city?.length ?: 0
+println("The length is $lenght")
+*/
+    //5.
+    fun scoreValue ( score: Double ): Double {
+return score ?: 0.0
+
+    }
+
+    println(" ${scoreValue (90.0)}")
+
 }
